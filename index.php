045af693b038d7169d97d72a1c87bfe62eb06928
genkyan <genkyan1104@gmail.com>
2019-01-15 18:11:55 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>注文システム</title>
+    <link rel="stylesheet" href="/css/master.css">
+  </head>
+  <body>
+    <header>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+    </footer>
+  </body>
+</html>
