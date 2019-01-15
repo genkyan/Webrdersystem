d0b790625b8f945904d707526d44cbb50fbb6fb8
genkyan <genkyan1104@gmail.com>
2019-01-15 19:23:50 +0900

    css追加

--- index.php
+++ index.php
@@ -3,13 +3,18 @@
   <head>
     <meta charset="utf-8">
     <title>注文システム</title>
-    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="./mainstylesheet.css">
   </head>
   <body>
     <header>
-     <div class="">
+     <div class="header-logo">
+       <a href="./index.php" style="height: auto; width: auto;">ぷりん</a>
+     </div>
+
+     <div class="main-navigation">
 
      </div>
+
     </header>
 
     <main>
